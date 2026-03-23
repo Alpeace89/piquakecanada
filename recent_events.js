7ace3cb91201d0944ed0aa9da93728f31afadad6
Alpeace89 <alpeace89@yahoo.com>
2026-03-23 15:27:21 -0400

    Auto update 2026-03-23 15:27:21

---
+++ recent_events.js
@@ -0,0 +1,32 @@
+window.recentEvents = [
+  {
+    "time_utc": "2026-03-22T21:09:19.000Z",
+    "magnitude": "1.15",
+    "depth_km": "11.43",
+    "place": "14 km E of Pembroke, ON/14 km E de Pembroke, ON"
+  },
+  {
+    "time_utc": "2026-03-22T19:23:30.000Z",
+    "magnitude": "4.28",
+    "depth_km": "10.0",
+    "place": "253 km SSW of Port Hardy, BC/253 km SSO de Port Hardy, BC"
+  },
+  {
+    "time_utc": "2026-03-22T06:59:27.000Z",
+    "magnitude": "2.89",
+    "depth_km": "1.0",
+    "place": "7 km S of Waskatenau, AB/7 km S de Waskatenau, AB"
+  },
+  {
+    "time_utc": "2026-03-22T05:16:20.000Z",
+    "magnitude": "0.05",
+    "depth_km": "5.11",
+    "place": "6 km NNW of La Malbaie, QC/6 km NNO de La Malbaie, QC"
+  },
+  {
+    "time_utc": "2026-03-21T16:06:04.000Z",
+    "magnitude": "3.08",
+    "depth_km": "0.0",
+    "place": "Blast, 17 km ESE of Hinton, AB/Dynamitage, 17 km ESE de Hinton, AB"
+  }
+];
