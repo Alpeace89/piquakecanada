b462844dfea49cb2d289afc0ae266f1886dc7806
Alpeace89 <alpeace89@yahoo.com>
2026-03-23 15:39:23 -0400

    Auto update 2026-03-23 15:39:23

--- recent_events.js
+++ recent_events.js
@@ -28,5 +28,35 @@ window.recentEvents = [
     "magnitude": "3.08",
     "depth_km": "0.0",
     "place": "Blast, 17 km ESE of Hinton, AB/Dynamitage, 17 km ESE de Hinton, AB"
+  },
+  {
+    "time_utc": "2026-03-21T13:08:03.000Z",
+    "magnitude": "2.71",
+    "depth_km": "8.93",
+    "place": "66 km S of Kingston, ON/66 km S de Kingston, ON"
+  },
+  {
+    "time_utc": "2026-03-21T07:19:23.000Z",
+    "magnitude": "0.63",
+    "depth_km": "5.0",
+    "place": "28 km ENE of Watertown, NY/28 km ENE de Watertown, NY"
+  },
+  {
+    "time_utc": "2026-03-20T17:19:45.000Z",
+    "magnitude": "2.82",
+    "depth_km": "5.0",
+    "place": "94 km SSW of Burwash Landing, YT/94 km SSO de Burwash Landing, YT"
+  },
+  {
+    "time_utc": "2026-03-20T17:09:51.000Z",
+    "magnitude": "2.9",
+    "depth_km": "5.0",
+    "place": "83 km SW of Burwash Landing, YT/83 km SO de Burwash Landing, YT"
+  },
+  {
+    "time_utc": "2026-03-20T09:09:20.000Z",
+    "magnitude": "2.42",
+    "depth_km": "5.0",
+    "place": "59 km WSW of Burwash Landing, YT/59 km OSO de Burwash Landing, YT"
   }
 ];
