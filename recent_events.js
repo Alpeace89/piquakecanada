39b7d2345fb74f1951206771c70de9a5996d047f
Alpeace89 <alpeace89@yahoo.com>
2026-03-25 11:46:17 -0400

    Auto update 2026-03-25 11:46:17

--- recent_events.js
+++ recent_events.js
@@ -6,6 +6,18 @@ window.recentEvents = [
     "place": "93 km SSE of Daajing Giids, BC/93 km SSE de Daajing Giids, BC"
   },
   {
+    "time_utc": "2026-03-24T14:25:36.000Z",
+    "magnitude": "3.02",
+    "depth_km": "10.0",
+    "place": "248 km SSW of Port Hardy, BC/248 km SSO de Port Hardy, BC"
+  },
+  {
+    "time_utc": "2026-03-24T09:10:24.000Z",
+    "magnitude": "2.66",
+    "depth_km": "1.0",
+    "place": "Suspected industry-related event, 59 km NE of Grande Cache, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 59 km NE de Grande Cache, AB"
+  },
+  {
     "time_utc": "2026-03-24T07:02:45.000Z",
     "magnitude": "2.51",
     "depth_km": "38.47",
@@ -46,17 +58,5 @@ window.recentEvents = [
     "magnitude": "2.53",
     "depth_km": "1.0",
     "place": "Suspected industry-related event, 28 km NE of Halfway River 168, BC/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 28 km NE de Halfway River 168, BC"
-  },
-  {
-    "time_utc": "2026-03-23T07:10:45.000Z",
-    "magnitude": "2.13",
-    "depth_km": "5.0",
-    "place": "58 km WSW of Burwash Landing, YT/58 km OSO de Burwash Landing, YT"
-  },
-  {
-    "time_utc": "2026-03-23T06:59:56.000Z",
-    "magnitude": "2.62",
-    "depth_km": "5.0",
-    "place": "58 km WSW of Burwash Landing, YT/58 km OSO de Burwash Landing, YT"
   }
 ];
