a2be42c2ec9392f56a5667aef7d532e399bb0db7
Alpeace89 <alpeace89@yahoo.com>
2026-03-30 18:21:06 -0400

    Auto update 2026-03-30 18:21:06

--- recent_events.js
+++ recent_events.js
@@ -25,13 +25,13 @@ window.recentEvents = [
   },
   {
     "time_utc": "2026-03-30T02:07:45.000Z",
-    "magnitude": "3.24",
+    "magnitude": "2.62",
     "depth_km": "10.0",
     "place": "229 km SSW of Port Hardy, BC/229 km SSO de Port Hardy, BC"
   },
   {
     "time_utc": "2026-03-30T02:07:45.000Z",
-    "magnitude": "2.62",
+    "magnitude": "3.24",
     "depth_km": "10.0",
     "place": "229 km SSW of Port Hardy, BC/229 km SSO de Port Hardy, BC"
   },
@@ -48,15 +48,15 @@ window.recentEvents = [
     "place": "52 km WSW of Burwash Landing, YT/52 km OSO de Burwash Landing, YT"
   },
   {
+    "time_utc": "2026-03-29T18:07:21.000Z",
+    "magnitude": "3.46",
+    "depth_km": "1.0",
+    "place": "249 km SW of Whitehorse, YT/249 km SO de Whitehorse, YT"
+  },
+  {
     "time_utc": "2026-03-29T17:50:18.000Z",
     "magnitude": "4.52",
     "depth_km": "10.0",
     "place": "204 km W of Port Hardy, BC/204 km O de Port Hardy, BC"
-  },
-  {
-    "time_utc": "2026-03-29T17:50:17.000Z",
-    "magnitude": "4.8",
-    "depth_km": "5.0",
-    "place": "Preliminary; 183 km W   of Port Hardy, BC/Preliminaire; 183 km O   de Port Hardy, BC"
   }
 ];
