05aab36006ae27329e44a96db58e8e66a3ce9b6a
Alpeace89 <alpeace89@yahoo.com>
2026-04-02 15:21:07 -0400

    Auto update 2026-04-02 15:21:07

--- recent_events.js
+++ recent_events.js
@@ -1,5 +1,11 @@
 window.recentEvents = [
   {
+    "time_utc": "2026-04-02T17:38:24.000Z",
+    "magnitude": "1.65",
+    "depth_km": "0.0",
+    "place": "Blast, 8 km S of Coaticook, QC/Dynamitage, 8 km S de Coaticook, QC"
+  },
+  {
     "time_utc": "2026-04-02T16:00:17.000Z",
     "magnitude": "1.36",
     "depth_km": "0.0",
@@ -52,11 +58,5 @@ window.recentEvents = [
     "magnitude": "1.51",
     "depth_km": "5.0",
     "place": "81 km SSE of Haines Junction, YT/81 km SSE de Haines Junction, YT"
-  },
-  {
-    "time_utc": "2026-04-02T01:02:08.000Z",
-    "magnitude": "2.33",
-    "depth_km": "1.0",
-    "place": "Suspected industry-related event, 61 km NE of Grande Cache, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 61 km NE de Grande Cache, AB"
   }
 ];
