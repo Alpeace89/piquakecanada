ef67de90520d20e87c187b1b631613a4dcbe7fdd
Alpeace89 <alpeace89@yahoo.com>
2026-04-08 15:21:05 -0400

    Auto update 2026-04-08 15:21:05

--- recent_events.js
+++ recent_events.js
@@ -1,9 +1,39 @@
 window.recentEvents = [
   {
+    "time_utc": "2026-04-08T17:41:46.000Z",
+    "magnitude": "1.75",
+    "depth_km": "5.45",
+    "place": "10 km SE of Dundee, QC/10 km SE de Dundee, QC"
+  },
+  {
+    "time_utc": "2026-04-08T17:35:12.000Z",
+    "magnitude": "1.23",
+    "depth_km": "0.0",
+    "place": "Blast, 3 km SSE of Colborne, ON, felt/Dynamitage, 3 km SSE de Colborne, ON, ressenti"
+  },
+  {
+    "time_utc": "2026-04-08T17:30:03.000Z",
+    "magnitude": "1.28",
+    "depth_km": "0.0",
+    "place": "Blast, 3 km S of Colborne, ON, felt/Dynamitage, 3 km S de Colborne, ON, ressenti"
+  },
+  {
     "time_utc": "2026-04-08T11:19:52.000Z",
     "magnitude": "3.97",
     "depth_km": "4.38",
     "place": "127 km SSW of Whitehorse, YT/127 km SSO de Whitehorse, YT"
+  },
+  {
+    "time_utc": "2026-04-07T13:15:30.000Z",
+    "magnitude": "1.03",
+    "depth_km": "9.63",
+    "place": "12 km SW of Lachute, QC/12 km SO de Lachute, QC"
+  },
+  {
+    "time_utc": "2026-04-07T09:44:31.000Z",
+    "magnitude": "1.07",
+    "depth_km": "2.0",
+    "place": "7 km WSW of Saint-Sim\u00e9on, QC/7 km OSO de Saint-Sim\u00e9on, QC"
   },
   {
     "time_utc": "2026-04-06T08:00:31.000Z",
@@ -28,35 +58,5 @@ window.recentEvents = [
     "magnitude": "1.06",
     "depth_km": "1.62",
     "place": "Mining event, 4 km WSW of Kirkland Lake, ON/\u00c9v\u00e9nement minier, 4 km OSO de Kirkland Lake, ON"
-  },
-  {
-    "time_utc": "2026-04-06T05:20:20.000Z",
-    "magnitude": "3.05",
-    "depth_km": "10.0",
-    "place": "198 km WSW of Port Hardy, BC/198 km OSO de Port Hardy, BC"
-  },
-  {
-    "time_utc": "2026-04-06T00:56:27.000Z",
-    "magnitude": "2.14",
-    "depth_km": "1.0",
-    "place": "Suspected blast, 27 km WNW of Kluskus 1, BC/Dynamitage soupconn\u00e9, 27 km ONO de Kluskus 1, BC"
-  },
-  {
-    "time_utc": "2026-04-05T22:21:48.000Z",
-    "magnitude": "1.86",
-    "depth_km": "5.0",
-    "place": "67 km ENE of Rochester, NY/67 km ENE de Rochester, NY"
-  },
-  {
-    "time_utc": "2026-04-05T09:20:03.000Z",
-    "magnitude": "0.73",
-    "depth_km": "9.17",
-    "place": "14 km WNW of Saint-Alexandre-de-Kamouraska, QC/14 km ONO de Saint-Alexandre-de-Kamouraska, QC"
-  },
-  {
-    "time_utc": "2026-04-05T07:29:44.000Z",
-    "magnitude": "1.29",
-    "depth_km": "5.0",
-    "place": "21 km SSW of Doaktown, NB/21 km SSO de Doaktown, NB"
   }
 ];
