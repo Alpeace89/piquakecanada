97be7fc03c4fb60d91bde38dd9b8db7d8dd749c9
Alpeace89 <alpeace89@yahoo.com>
2026-04-13 15:21:02 -0400

    Auto update 2026-04-13 15:21:02

--- recent_events.js
+++ recent_events.js
@@ -12,10 +12,22 @@ window.recentEvents = [
     "place": "416 km SW of Courtenay, BC/416 km SO de Courtenay, BC"
   },
   {
+    "time_utc": "2026-04-12T13:25:24.000Z",
+    "magnitude": "3.43",
+    "depth_km": "10.0",
+    "place": "193 km W of Port Hardy, BC/193 km O de Port Hardy, BC"
+  },
+  {
     "time_utc": "2026-04-12T11:38:24.000Z",
     "magnitude": "0.63",
     "depth_km": "15.25",
     "place": "5 km NW of La Pocati\u00e8re, QC/5 km NO de La Pocati\u00e8re, QC"
+  },
+  {
+    "time_utc": "2026-04-12T11:13:36.000Z",
+    "magnitude": "2.38",
+    "depth_km": "22.4",
+    "place": "123 km S of Daajing Giids, BC/123 km S de Daajing Giids, BC"
   },
   {
     "time_utc": "2026-04-12T07:18:21.000Z",
@@ -36,27 +48,15 @@ window.recentEvents = [
     "place": "49 km N of Sainte-Anne-des-Monts, QC/49 km N de Sainte-Anne-des-Monts, QC"
   },
   {
+    "time_utc": "2026-04-11T21:10:44.000Z",
+    "magnitude": "1.4",
+    "depth_km": "5.0",
+    "place": "38 km W of Doaktown, NB/38 km O de Doaktown, NB"
+  },
+  {
     "time_utc": "2026-04-11T14:17:03.000Z",
     "magnitude": "1.1",
     "depth_km": "2.0",
     "place": "Suspected industry-related event, 34 km NNW of Lac Tio Mine, QC/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 34 km NNO de la mine Lac Tio, QC"
-  },
-  {
-    "time_utc": "2026-04-11T10:04:58.000Z",
-    "magnitude": "1.06",
-    "depth_km": "5.0",
-    "place": "66 km NNE of Hauterive, QC/66 km NNE de Hauterive, QC"
-  },
-  {
-    "time_utc": "2026-04-11T07:55:25.000Z",
-    "magnitude": "0.74",
-    "depth_km": "5.0",
-    "place": "25 km NNE of Chipman, NB/25 km NNE de Chipman, NB"
-  },
-  {
-    "time_utc": "2026-04-11T07:13:56.000Z",
-    "magnitude": "1.7",
-    "depth_km": "8.61",
-    "place": "3 km N of Dundas, NB/3 km N de Dundas, NB"
   }
 ];
