ba5a21a39153d05baf3d5a781400f1d399cf62b4
Alpeace89 <alpeace89@yahoo.com>
2026-04-13 13:21:06 -0400

    Auto update 2026-04-13 13:21:06

--- recent_events.js
+++ recent_events.js
@@ -12,10 +12,40 @@ window.recentEvents = [
     "place": "5 km NW of La Pocati\u00e8re, QC/5 km NO de La Pocati\u00e8re, QC"
   },
   {
+    "time_utc": "2026-04-12T07:18:21.000Z",
+    "magnitude": "3.22",
+    "depth_km": "5.0",
+    "place": "137 km W of Haines Junction, YT/137 km O de Haines Junction, YT"
+  },
+  {
+    "time_utc": "2026-04-12T06:52:37.000Z",
+    "magnitude": "4.16",
+    "depth_km": "5.0",
+    "place": "136 km W of Haines Junction, YT/136 km O de Haines Junction, YT"
+  },
+  {
     "time_utc": "2026-04-12T02:07:09.000Z",
     "magnitude": "0.8",
     "depth_km": "18.0",
     "place": "49 km N of Sainte-Anne-des-Monts, QC/49 km N de Sainte-Anne-des-Monts, QC"
+  },
+  {
+    "time_utc": "2026-04-11T07:55:25.000Z",
+    "magnitude": "0.74",
+    "depth_km": "5.0",
+    "place": "25 km NNE of Chipman, NB/25 km NNE de Chipman, NB"
+  },
+  {
+    "time_utc": "2026-04-11T07:13:56.000Z",
+    "magnitude": "1.7",
+    "depth_km": "8.61",
+    "place": "3 km N of Dundas, NB/3 km N de Dundas, NB"
+  },
+  {
+    "time_utc": "2026-04-11T04:54:13.000Z",
+    "magnitude": "0.41",
+    "depth_km": "21.4",
+    "place": "5 km NW of La Pocati\u00e8re, QC/5 km NO de La Pocati\u00e8re, QC"
   },
   {
     "time_utc": "2026-04-10T23:12:33.000Z",
@@ -28,35 +58,5 @@ window.recentEvents = [
     "magnitude": "1.64",
     "depth_km": "1.0",
     "place": "19 km WSW of Coal Harbour, BC/19 km OSO de Coal Harbour, BC"
-  },
-  {
-    "time_utc": "2026-04-10T20:44:11.000Z",
-    "magnitude": "2.87",
-    "depth_km": "1.0",
-    "place": "Suspected industry-related event, 62 km NE of Grande Cache, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 62 km NE de Grande Cache, AB"
-  },
-  {
-    "time_utc": "2026-04-10T20:26:58.000Z",
-    "magnitude": "2.85",
-    "depth_km": "1.0",
-    "place": "Suspected industry-related event, 65 km NE of Grande Cache, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 65 km NE de Grande Cache, AB"
-  },
-  {
-    "time_utc": "2026-04-10T17:44:30.000Z",
-    "magnitude": "2.61",
-    "depth_km": "1.0",
-    "place": "Suspected industry-related event, 27 km W of Fox Creek, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 27 km O de Fox Creek, AB"
-  },
-  {
-    "time_utc": "2026-04-10T16:43:17.000Z",
-    "magnitude": "4.57",
-    "depth_km": "5.0",
-    "place": "141 km WSW of Juneau, AK/141 km OSO de Juneau, AK"
-  },
-  {
-    "time_utc": "2026-04-10T11:47:43.000Z",
-    "magnitude": "1.96",
-    "depth_km": "5.0",
-    "place": "140 km W of Haines Junction, YT/140 km O de Haines Junction, YT"
   }
 ];
