2798eb97b56cc190bf5ecf123d159541ad1c72f5
Alpeace89 <alpeace89@yahoo.com>
2026-04-14 11:21:04 -0400

    Auto update 2026-04-14 11:21:04

--- recent_events.js
+++ recent_events.js
@@ -1,5 +1,53 @@
 window.recentEvents = [
   {
+    "time_utc": "2026-04-13T16:16:28.000Z",
+    "magnitude": "2.86",
+    "depth_km": "5.0",
+    "place": "253 km WSW of Haines Junction, YT/253 km OSO de Haines Junction, YT"
+  },
+  {
+    "time_utc": "2026-04-13T13:41:30.000Z",
+    "magnitude": "2.19",
+    "depth_km": "5.0",
+    "place": "133 km SW of Whitehorse, YT/133 km SO de Whitehorse, YT"
+  },
+  {
+    "time_utc": "2026-04-13T12:26:16.000Z",
+    "magnitude": "1.9",
+    "depth_km": "5.0",
+    "place": "128 km SSW of Whitehorse, YT/128 km SSO de Whitehorse, YT"
+  },
+  {
+    "time_utc": "2026-04-13T08:24:06.000Z",
+    "magnitude": "3.26",
+    "depth_km": "5.0",
+    "place": "263 km WSW of Haines Junction, YT/263 km OSO de Haines Junction, YT"
+  },
+  {
+    "time_utc": "2026-04-13T07:30:13.000Z",
+    "magnitude": "2.07",
+    "depth_km": "1.0",
+    "place": "Suspected industry-related event, 23 km SE of Halfway River 168, BC/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 23 km SE de Halfway River 168, BC"
+  },
+  {
+    "time_utc": "2026-04-13T07:03:02.000Z",
+    "magnitude": "2.17",
+    "depth_km": "5.0",
+    "place": "105 km SW of Burwash Landing, YT/105 km SO de Burwash Landing, YT"
+  },
+  {
+    "time_utc": "2026-04-13T04:53:50.000Z",
+    "magnitude": "3.08",
+    "depth_km": "5.0",
+    "place": "138 km W of Haines Junction, YT/138 km O de Haines Junction, YT"
+  },
+  {
+    "time_utc": "2026-04-13T04:51:07.000Z",
+    "magnitude": "2.28",
+    "depth_km": "1.0",
+    "place": "Suspected industry-related event, 58 km W of Fox Creek, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 58 km O de Fox Creek, AB"
+  },
+  {
     "time_utc": "2026-04-13T03:14:06.000Z",
     "magnitude": "2.47",
     "depth_km": "5.0",
@@ -10,53 +58,5 @@ window.recentEvents = [
     "magnitude": "1.98",
     "depth_km": "5.0",
     "place": "139 km WSW of Haines Junction, YT/139 km OSO de Haines Junction, YT"
-  },
-  {
-    "time_utc": "2026-04-12T18:11:53.000Z",
-    "magnitude": "1.41",
-    "depth_km": "5.0",
-    "place": "58 km SE of Oshawa, ON/58 km SE de Oshawa, ON"
-  },
-  {
-    "time_utc": "2026-04-12T16:04:57.000Z",
-    "magnitude": "3.85",
-    "depth_km": "10.0",
-    "place": "416 km SW of Courtenay, BC/416 km SO de Courtenay, BC"
-  },
-  {
-    "time_utc": "2026-04-12T13:25:24.000Z",
-    "magnitude": "3.43",
-    "depth_km": "10.0",
-    "place": "193 km W of Port Hardy, BC/193 km O de Port Hardy, BC"
-  },
-  {
-    "time_utc": "2026-04-12T11:38:24.000Z",
-    "magnitude": "0.63",
-    "depth_km": "15.25",
-    "place": "5 km NW of La Pocati\u00e8re, QC/5 km NO de La Pocati\u00e8re, QC"
-  },
-  {
-    "time_utc": "2026-04-12T11:13:36.000Z",
-    "magnitude": "2.38",
-    "depth_km": "22.4",
-    "place": "123 km S of Daajing Giids, BC/123 km S de Daajing Giids, BC"
-  },
-  {
-    "time_utc": "2026-04-12T07:18:21.000Z",
-    "magnitude": "3.22",
-    "depth_km": "5.0",
-    "place": "137 km W of Haines Junction, YT/137 km O de Haines Junction, YT"
-  },
-  {
-    "time_utc": "2026-04-12T06:52:37.000Z",
-    "magnitude": "4.16",
-    "depth_km": "5.0",
-    "place": "136 km W of Haines Junction, YT/136 km O de Haines Junction, YT"
-  },
-  {
-    "time_utc": "2026-04-12T02:07:09.000Z",
-    "magnitude": "0.8",
-    "depth_km": "18.0",
-    "place": "49 km N of Sainte-Anne-des-Monts, QC/49 km N de Sainte-Anne-des-Monts, QC"
   }
 ];
