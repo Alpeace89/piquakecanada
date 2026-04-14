c57410e9e6b9bb0706602698d59caa707aa6e290
Alpeace89 <alpeace89@yahoo.com>
2026-04-14 14:21:06 -0400

    Auto update 2026-04-14 14:21:06

--- recent_events.js
+++ recent_events.js
@@ -2,8 +2,32 @@ window.recentEvents = [
   {
     "time_utc": "2026-04-14T16:36:43.000Z",
     "magnitude": "3.94",
+    "depth_km": "13.17",
+    "place": "12 km SSE of Otter Lake, QC/12 km SSE de Otter Lake, QC"
+  },
+  {
+    "time_utc": "2026-04-14T16:36:43.000Z",
+    "magnitude": "3.94",
     "depth_km": "12.94",
     "place": "12 km SSE of Otter Lake, QC/12 km SSE de Otter Lake, QC"
+  },
+  {
+    "time_utc": "2026-04-14T15:03:08.000Z",
+    "magnitude": "1.56",
+    "depth_km": "29.5",
+    "place": "32 km WSW of Yuquot 1, BC/32 km OSO de Yuquot 1, BC"
+  },
+  {
+    "time_utc": "2026-04-14T12:57:24.000Z",
+    "magnitude": "2.72",
+    "depth_km": "0.98",
+    "place": "83 km WNW of Burwash Landing, YT/83 km ONO de Burwash Landing, YT"
+  },
+  {
+    "time_utc": "2026-04-14T06:16:35.000Z",
+    "magnitude": "2.28",
+    "depth_km": "2.19",
+    "place": "130 km SW of Whitehorse, YT/130 km SO de Whitehorse, YT"
   },
   {
     "time_utc": "2026-04-13T23:11:33.000Z",
@@ -34,29 +58,5 @@ window.recentEvents = [
     "magnitude": "2.19",
     "depth_km": "5.0",
     "place": "133 km SW of Whitehorse, YT/133 km SO de Whitehorse, YT"
-  },
-  {
-    "time_utc": "2026-04-13T12:26:16.000Z",
-    "magnitude": "1.9",
-    "depth_km": "5.0",
-    "place": "128 km SSW of Whitehorse, YT/128 km SSO de Whitehorse, YT"
-  },
-  {
-    "time_utc": "2026-04-13T08:24:06.000Z",
-    "magnitude": "3.26",
-    "depth_km": "5.0",
-    "place": "263 km WSW of Haines Junction, YT/263 km OSO de Haines Junction, YT"
-  },
-  {
-    "time_utc": "2026-04-13T07:30:13.000Z",
-    "magnitude": "2.07",
-    "depth_km": "1.0",
-    "place": "Suspected industry-related event, 23 km SE of Halfway River 168, BC/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 23 km SE de Halfway River 168, BC"
-  },
-  {
-    "time_utc": "2026-04-13T07:03:02.000Z",
-    "magnitude": "2.17",
-    "depth_km": "5.0",
-    "place": "105 km SW of Burwash Landing, YT/105 km SO de Burwash Landing, YT"
   }
 ];
