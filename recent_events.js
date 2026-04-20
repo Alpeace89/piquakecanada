07e63ed414bd0a4435ff8e2bcd0ccf4d8deaba30
Alpeace89 <alpeace89@yahoo.com>
2026-04-20 09:21:04 -0400

    Auto update 2026-04-20 09:21:04

--- recent_events.js
+++ recent_events.js
@@ -12,6 +12,12 @@ window.recentEvents = [
     "place": "11 km NNW of Saint-Pascal, QC/11 km NNO de Saint-Pascal, QC"
   },
   {
+    "time_utc": "2026-04-16T21:59:36.000Z",
+    "magnitude": "1.14",
+    "depth_km": "5.0",
+    "place": "58 km E of Bangor, ME/58 km E de Bangor, ME"
+  },
+  {
     "time_utc": "2026-04-16T21:08:17.000Z",
     "magnitude": "2.72",
     "depth_km": "1.0",
@@ -42,21 +48,15 @@ window.recentEvents = [
     "place": "Blast, 11 km NNW of Orillia, ON/Dynamitage, 11 km NNO de Orillia, ON"
   },
   {
+    "time_utc": "2026-04-16T14:57:21.000Z",
+    "magnitude": "1.36",
+    "depth_km": "0.0",
+    "place": "Blast, 7 km ESE of Saint-Joseph-de-Beauce, QC/Dynamitage, 7 km ESE de Saint-Joseph-de-Beauce, QC"
+  },
+  {
     "time_utc": "2026-04-16T14:15:15.000Z",
     "magnitude": "2.1",
     "depth_km": "5.0",
     "place": "123 km WSW of Haines Junction, YT/123 km OSO de Haines Junction, YT"
-  },
-  {
-    "time_utc": "2026-04-16T14:02:15.000Z",
-    "magnitude": "2.16",
-    "depth_km": "1.0",
-    "place": "Suspected industry-related event, 28 km N of Halfway River 168, BC/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 28 km N de Halfway River 168, BC"
-  },
-  {
-    "time_utc": "2026-04-16T11:14:16.000Z",
-    "magnitude": "2.96",
-    "depth_km": "5.0",
-    "place": "140 km W of Haines Junction, YT/140 km O de Haines Junction, YT"
   }
 ];
