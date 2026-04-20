209d225467907ca279ad55a2e5cda493ffa69596
Alpeace89 <alpeace89@yahoo.com>
2026-04-20 12:21:05 -0400

    Auto update 2026-04-20 12:21:05

--- recent_events.js
+++ recent_events.js
@@ -6,6 +6,18 @@ window.recentEvents = [
     "place": "40 km WNW of Cap-Chat, QC/40 km ONO de Cap-Chat, QC"
   },
   {
+    "time_utc": "2026-04-17T13:28:43.000Z",
+    "magnitude": "3.06",
+    "depth_km": "10.0",
+    "place": "198 km W of Port Hardy, BC/198 km O de Port Hardy, BC"
+  },
+  {
+    "time_utc": "2026-04-17T12:43:14.000Z",
+    "magnitude": "3.19",
+    "depth_km": "10.0",
+    "place": "260 km WNW of Port Hardy, BC/260 km ONO de Port Hardy, BC"
+  },
+  {
     "time_utc": "2026-04-17T12:41:21.000Z",
     "magnitude": "3.41",
     "depth_km": "10.0",
@@ -46,17 +58,5 @@ window.recentEvents = [
     "magnitude": "1.65",
     "depth_km": "5.0",
     "place": "34 km SW of Chilco Lake 1A, BC/34 km SO de Chilco Lake 1A, BC"
-  },
-  {
-    "time_utc": "2026-04-16T21:59:36.000Z",
-    "magnitude": "1.14",
-    "depth_km": "5.0",
-    "place": "58 km E of Bangor, ME/58 km E de Bangor, ME"
-  },
-  {
-    "time_utc": "2026-04-16T21:08:17.000Z",
-    "magnitude": "2.72",
-    "depth_km": "1.0",
-    "place": "Suspected industry-related event, 63 km SSE of Grande Prairie, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 63 km SSE de Grande Prairie, AB"
   }
 ];
