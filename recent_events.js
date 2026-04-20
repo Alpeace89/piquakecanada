14e62482f19939f5864e0f592ea20a791fc8ca96
Alpeace89 <alpeace89@yahoo.com>
2026-04-20 10:21:05 -0400

    Auto update 2026-04-20 10:21:05

--- recent_events.js
+++ recent_events.js
@@ -1,15 +1,33 @@
 window.recentEvents = [
   {
+    "time_utc": "2026-04-19T10:57:56.000Z",
+    "magnitude": "1.24",
+    "depth_km": "17.24",
+    "place": "40 km WNW of Cap-Chat, QC/40 km ONO de Cap-Chat, QC"
+  },
+  {
     "time_utc": "2026-04-17T03:37:58.000Z",
     "magnitude": "-0.02",
     "depth_km": "11.33",
     "place": "11 km SSE of La Malbaie, QC/11 km SSE de La Malbaie, QC"
   },
   {
+    "time_utc": "2026-04-17T03:31:18.000Z",
+    "magnitude": "2.25",
+    "depth_km": "5.0",
+    "place": "127 km W of Haines Junction, YT/127 km O de Haines Junction, YT"
+  },
+  {
     "time_utc": "2026-04-17T02:55:01.000Z",
     "magnitude": "0.45",
     "depth_km": "16.29",
     "place": "11 km NNW of Saint-Pascal, QC/11 km NNO de Saint-Pascal, QC"
+  },
+  {
+    "time_utc": "2026-04-17T02:42:40.000Z",
+    "magnitude": "1.65",
+    "depth_km": "5.0",
+    "place": "34 km SW of Chilco Lake 1A, BC/34 km SO de Chilco Lake 1A, BC"
   },
   {
     "time_utc": "2026-04-16T21:59:36.000Z",
@@ -40,23 +58,5 @@ window.recentEvents = [
     "magnitude": "3.67",
     "depth_km": "10.0",
     "place": "198 km W of Port Hardy, BC/198 km O de Port Hardy, BC"
-  },
-  {
-    "time_utc": "2026-04-16T15:33:25.000Z",
-    "magnitude": "1.1",
-    "depth_km": "0.0",
-    "place": "Blast, 11 km NNW of Orillia, ON/Dynamitage, 11 km NNO de Orillia, ON"
-  },
-  {
-    "time_utc": "2026-04-16T14:57:21.000Z",
-    "magnitude": "1.36",
-    "depth_km": "0.0",
-    "place": "Blast, 7 km ESE of Saint-Joseph-de-Beauce, QC/Dynamitage, 7 km ESE de Saint-Joseph-de-Beauce, QC"
-  },
-  {
-    "time_utc": "2026-04-16T14:15:15.000Z",
-    "magnitude": "2.1",
-    "depth_km": "5.0",
-    "place": "123 km WSW of Haines Junction, YT/123 km OSO de Haines Junction, YT"
   }
 ];
