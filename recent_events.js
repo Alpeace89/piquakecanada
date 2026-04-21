77b323eca89279424dc43182b8082f60be007175
Alpeace89 <alpeace89@yahoo.com>
2026-04-21 11:21:05 -0400

    Auto update 2026-04-21 11:21:05

--- recent_events.js
+++ recent_events.js
@@ -1,5 +1,29 @@
 window.recentEvents = [
   {
+    "time_utc": "2026-04-20T16:59:01.000Z",
+    "magnitude": "3.42",
+    "depth_km": "10.0",
+    "place": "243 km SSW of Port Hardy, BC/243 km SSO de Port Hardy, BC"
+  },
+  {
+    "time_utc": "2026-04-20T16:37:01.000Z",
+    "magnitude": "3.19",
+    "depth_km": "10.0",
+    "place": "237 km SSW of Port Hardy, BC/237 km SSO de Port Hardy, BC"
+  },
+  {
+    "time_utc": "2026-04-20T15:43:55.000Z",
+    "magnitude": "3.62",
+    "depth_km": "10.0",
+    "place": "239 km SSW of Port Hardy, BC/239 km SSO de Port Hardy, BC"
+  },
+  {
+    "time_utc": "2026-04-20T14:53:54.000Z",
+    "magnitude": "2.36",
+    "depth_km": "5.0",
+    "place": "135 km W of Haines Junction, YT/135 km O de Haines Junction, YT"
+  },
+  {
     "time_utc": "2026-04-20T13:44:23.000Z",
     "magnitude": "2.12",
     "depth_km": "1.0",
@@ -34,29 +58,5 @@ window.recentEvents = [
     "magnitude": "2.4",
     "depth_km": "5.0",
     "place": "136 km W of Haines Junction, YT/136 km O de Haines Junction, YT"
-  },
-  {
-    "time_utc": "2026-04-20T07:33:27.000Z",
-    "magnitude": "2.14",
-    "depth_km": "5.0",
-    "place": "100 km SSW of Burwash Landing, YT/100 km SSO de Burwash Landing, YT"
-  },
-  {
-    "time_utc": "2026-04-20T04:46:25.000Z",
-    "magnitude": "1.98",
-    "depth_km": "1.0",
-    "place": "12 km N of Port Clements, BC/12 km N de Port Clements, BC"
-  },
-  {
-    "time_utc": "2026-04-20T04:18:46.000Z",
-    "magnitude": "2.17",
-    "depth_km": "10.0",
-    "place": "53 km SW of Port Alice, BC/53 km SO de Port Alice, BC"
-  },
-  {
-    "time_utc": "2026-04-19T15:36:54.000Z",
-    "magnitude": "3.62",
-    "depth_km": "18.26",
-    "place": "76 km W of Daajing Giids, BC/76 km O de Daajing Giids, BC"
   }
 ];
