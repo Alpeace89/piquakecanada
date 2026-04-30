e2621372d0631bbf89f35169e44e83d02f487b05
Alpeace89 <alpeace89@yahoo.com>
2026-04-30 15:21:05 -0400

    Auto update 2026-04-30 15:21:05

--- recent_events.js
+++ recent_events.js
@@ -1,9 +1,21 @@
 window.recentEvents = [
   {
+    "time_utc": "2026-04-30T15:53:26.000Z",
+    "magnitude": "1.94",
+    "depth_km": "13.44",
+    "place": "13 km SSE of Otter Lake, QC, felt/13 km SSE de Otter Lake, QC, ressenti"
+  },
+  {
     "time_utc": "2026-04-30T01:05:39.000Z",
     "magnitude": "4.12",
     "depth_km": "5.0",
     "place": "119 km WSW of Haines Junction, YT/119 km OSO de Haines Junction, YT"
+  },
+  {
+    "time_utc": "2026-04-29T22:41:08.000Z",
+    "magnitude": "2.74",
+    "depth_km": "10.0",
+    "place": "253 km SW of Port Hardy, BC/253 km SO de Port Hardy, BC"
   },
   {
     "time_utc": "2026-04-29T09:19:54.000Z",
@@ -30,6 +42,12 @@ window.recentEvents = [
     "place": "16 km WSW of Amherst, QC/16 km OSO de Amherst, QC"
   },
   {
+    "time_utc": "2026-04-28T23:46:52.000Z",
+    "magnitude": "2.32",
+    "depth_km": "0.0",
+    "place": "Blast, 14 km NNE of Cariboo D, BC/Dynamitage, 14 km NNE de Cariboo D, BC"
+  },
+  {
     "time_utc": "2026-04-28T23:14:16.000Z",
     "magnitude": "3.15",
     "depth_km": "10.0",
@@ -40,23 +58,5 @@ window.recentEvents = [
     "magnitude": "1.54",
     "depth_km": "0.0",
     "place": "Blast, 4 km SSW of Thetford Mines, QC/Dynamitage, 4 km SSO de Thetford Mines, QC"
-  },
-  {
-    "time_utc": "2026-04-28T21:22:36.000Z",
-    "magnitude": "3.16",
-    "depth_km": "0.0",
-    "place": "Blast, 16 km E of Hinton, AB/Dynamitage, 16 km E de Hinton, AB"
-  },
-  {
-    "time_utc": "2026-04-28T16:28:47.000Z",
-    "magnitude": "2.67",
-    "depth_km": "3.17",
-    "place": "190 km SSW of Haines Junction, YT/190 km SSO de Haines Junction, YT"
-  },
-  {
-    "time_utc": "2026-04-28T15:22:22.000Z",
-    "magnitude": "2.36",
-    "depth_km": "5.0",
-    "place": "113 km WSW of Haines Junction, YT/113 km OSO de Haines Junction, YT"
   }
 ];
