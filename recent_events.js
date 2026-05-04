fa22a4e0153c5ca73f2a48a70b7cf1a95fd6e09c
Alpeace89 <alpeace89@yahoo.com>
2026-05-04 15:21:04 -0400

    Auto update 2026-05-04 15:21:04

--- recent_events.js
+++ recent_events.js
@@ -1,5 +1,11 @@
 window.recentEvents = [
   {
+    "time_utc": "2026-05-03T16:53:01.000Z",
+    "magnitude": "3.1",
+    "depth_km": "1.0",
+    "place": "58 km N of Juneau, AK/58 km N de Juneau, AK"
+  },
+  {
     "time_utc": "2026-05-03T13:55:48.000Z",
     "magnitude": "3.43",
     "depth_km": "5.5",
@@ -24,39 +30,33 @@ window.recentEvents = [
     "place": "2 km ESE of Ramara, ON/2 km ESE de Ramara, ON"
   },
   {
-    "time_utc": "2026-05-01T23:57:36.000Z",
-    "magnitude": "2.73",
-    "depth_km": "1.0",
-    "place": "Suspected industry-related event, 68 km NE of Grande Cache, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 68 km NE de Grande Cache, AB"
+    "time_utc": "2026-05-03T05:00:38.000Z",
+    "magnitude": "2.82",
+    "depth_km": "10.0",
+    "place": "144 km W of Marktosis 15, BC/144 km O de Marktosis 15, BC"
   },
   {
-    "time_utc": "2026-05-01T21:31:36.000Z",
-    "magnitude": "2.62",
-    "depth_km": "5.0",
-    "place": "88 km SW of Carcross, YT/88 km SO de Carcross, YT"
+    "time_utc": "2026-05-02T23:07:49.000Z",
+    "magnitude": "1.39",
+    "depth_km": "16.43",
+    "place": "1 km SE of Lachute, QC/1 km SE de Lachute, QC"
   },
   {
-    "time_utc": "2026-05-01T17:53:42.000Z",
-    "magnitude": "2.74",
-    "depth_km": "5.0",
-    "place": "50 km WSW of Beaver Creek, YT/50 km OSO de Beaver Creek, YT"
+    "time_utc": "2026-05-02T09:44:47.000Z",
+    "magnitude": "1.97",
+    "depth_km": "18.0",
+    "place": "39 km E of Hardwicke, NB/39 km E de Hardwicke, NB"
   },
   {
-    "time_utc": "2026-05-01T17:09:27.000Z",
-    "magnitude": "1.32",
-    "depth_km": "0.0",
-    "place": "Blast, 75 km ENE of Atikokan, ON/Dynamitage, 75 km ENE de Atikokan, ON"
+    "time_utc": "2026-05-02T08:30:42.000Z",
+    "magnitude": "1.4",
+    "depth_km": "6.13",
+    "place": "36 km WSW of St. Stephen, NB/36 km OSO de St. Stephen, NB"
   },
   {
-    "time_utc": "2026-05-01T16:48:32.000Z",
-    "magnitude": "1.12",
-    "depth_km": "0.0",
-    "place": "Suspected blast, 21 km NNW of Beaupr\u00e9, QC/Dynamitage soupconn\u00e9, 21 km NNO de Beaupr\u00e9, QC"
-  },
-  {
-    "time_utc": "2026-05-01T16:06:08.000Z",
-    "magnitude": "1.69",
-    "depth_km": "31.37",
-    "place": "37 km SW of Tahsis, BC/37 km SO de Tahsis, BC"
+    "time_utc": "2026-05-02T03:21:05.000Z",
+    "magnitude": "0.54",
+    "depth_km": "12.56",
+    "place": "15 km SSW of La Malbaie, QC/15 km SSO de La Malbaie, QC"
   }
 ];
