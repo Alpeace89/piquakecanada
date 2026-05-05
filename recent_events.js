57485c207ae07e9f341aeb7a9379499eeeb351d5
Alpeace89 <alpeace89@yahoo.com>
2026-05-05 17:21:05 -0400

    Auto update 2026-05-05 17:21:05

--- recent_events.js
+++ recent_events.js
@@ -1,9 +1,21 @@
 window.recentEvents = [
   {
+    "time_utc": "2026-05-05T14:45:16.000Z",
+    "magnitude": "2.56",
+    "depth_km": "2.55",
+    "place": "Suspected industry-related event, 59 km S of Grande Prairie, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 59 km S de Grande Prairie, AB"
+  },
+  {
     "time_utc": "2026-05-05T13:25:00.000Z",
     "magnitude": "3.52",
     "depth_km": "10.0",
     "place": "257 km WSW of Tofino, BC/257 km OSO de Tofino, BC"
+  },
+  {
+    "time_utc": "2026-05-05T06:21:11.000Z",
+    "magnitude": "2.45",
+    "depth_km": "5.8",
+    "place": "Suspected industry-related event, 63 km NE of Grande Cache, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 63 km NE de Grande Cache, AB"
   },
   {
     "time_utc": "2026-05-05T04:46:19.000Z",
@@ -46,17 +58,5 @@ window.recentEvents = [
     "magnitude": "3.07",
     "depth_km": "10.0",
     "place": "180 km SW of Port Hardy, BC/180 km SO de Port Hardy, BC"
-  },
-  {
-    "time_utc": "2026-05-04T01:42:48.000Z",
-    "magnitude": "2.05",
-    "depth_km": "5.0",
-    "place": "64 km WSW of Burwash Landing, YT/64 km OSO de Burwash Landing, YT"
-  },
-  {
-    "time_utc": "2026-05-04T00:52:13.000Z",
-    "magnitude": "2.11",
-    "depth_km": "1.0",
-    "place": "Suspected industry-related event, 20 km N of Halfway River 168, BC/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 20 km N de Halfway River 168, BC"
   }
 ];
