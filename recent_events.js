5750bf83d7afda997d3840ef1582f4ea085ab5ed
Alpeace89 <alpeace89@yahoo.com>
2026-05-13 11:21:06 -0400

    Auto update 2026-05-13 11:21:06

--- recent_events.js
+++ recent_events.js
@@ -1,62 +1,62 @@
 window.recentEvents = [
   {
-    "time_utc": "2026-05-10T22:45:18.000Z",
-    "magnitude": "4.04",
-    "depth_km": "21.61",
-    "place": "44 km SW of Daajing Giids, BC/44 km SO de Daajing Giids, BC"
+    "time_utc": "2026-05-12T20:35:23.000Z",
+    "magnitude": "3.95",
+    "depth_km": "9.82",
+    "place": "26 km NE of Skidegate 1, BC, felt/26 km NE de Skidegate 1, BC, ressenti"
   },
   {
-    "time_utc": "2026-05-09T15:05:04.000Z",
-    "magnitude": "4.0",
-    "depth_km": "2.0",
-    "place": "Preliminary; 54 km ENE of Fort Saskatchewan, AB/Preliminaire; 54 km ENE de Fort Saskatchewan, AB"
-  },
-  {
-    "time_utc": "2026-05-08T18:20:53.000Z",
-    "magnitude": "2.37",
-    "depth_km": "5.0",
-    "place": "136 km W of Haines Junction, YT/136 km O de Haines Junction, YT"
-  },
-  {
-    "time_utc": "2026-05-08T15:19:33.000Z",
-    "magnitude": "2.91",
-    "depth_km": "5.0",
-    "place": "100 km SSW of Burwash Landing, YT/100 km SSO de Burwash Landing, YT"
-  },
-  {
-    "time_utc": "2026-05-08T08:58:10.000Z",
-    "magnitude": "2.46",
+    "time_utc": "2026-05-12T17:05:29.000Z",
+    "magnitude": "3.07",
     "depth_km": "10.0",
-    "place": "177 km W of Tofino, BC/177 km O de Tofino, BC"
+    "place": "222 km WSW of Juneau, AK/222 km OSO de Juneau, AK"
   },
   {
-    "time_utc": "2026-05-08T01:10:22.000Z",
-    "magnitude": "1.45",
+    "time_utc": "2026-05-12T15:36:28.000Z",
+    "magnitude": "2.47",
     "depth_km": "1.0",
-    "place": "17 km N of Sliammon 1, BC/17 km N de Sliammon 1, BC"
-  },
-  {
-    "time_utc": "2026-05-07T23:59:25.000Z",
-    "magnitude": "2.9",
-    "depth_km": "5.0",
-    "place": "139 km W of Haines Junction, YT/139 km O de Haines Junction, YT"
-  },
-  {
-    "time_utc": "2026-05-07T19:45:52.000Z",
-    "magnitude": "0.19",
-    "depth_km": "8.75",
-    "place": "6 km NNE of La Malbaie, QC/6 km NNE de La Malbaie, QC"
-  },
-  {
-    "time_utc": "2026-05-07T17:37:35.000Z",
-    "magnitude": "2.72",
-    "depth_km": "5.0",
     "place": "62 km WSW of Burwash Landing, YT/62 km OSO de Burwash Landing, YT"
   },
   {
-    "time_utc": "2026-05-07T17:18:08.000Z",
-    "magnitude": "2.6",
+    "time_utc": "2026-05-11T22:54:16.000Z",
+    "magnitude": "2.1",
+    "depth_km": "1.0",
+    "place": "Blast, 2 km W of Golden, ON/Dynamitage, 2 km O de Golden, ON"
+  },
+  {
+    "time_utc": "2026-05-11T16:24:34.000Z",
+    "magnitude": "2.38",
+    "depth_km": "0.0",
+    "place": "Blast, 3 km NNE of Fermont, QC/Dynamitage, 3 km NNE de Fermont, QC"
+  },
+  {
+    "time_utc": "2026-05-11T16:13:48.000Z",
+    "magnitude": "0.72",
+    "depth_km": "2.0",
+    "place": "16 km N of McKellar, ON/16 km N de McKellar, ON"
+  },
+  {
+    "time_utc": "2026-05-11T15:58:53.000Z",
+    "magnitude": "1.46",
+    "depth_km": "2.0",
+    "place": "19 km N of McKellar, ON/19 km N de McKellar, ON"
+  },
+  {
+    "time_utc": "2026-05-11T10:38:10.000Z",
+    "magnitude": "1.14",
+    "depth_km": "21.88",
+    "place": "29 km NE of Saanich, BC/29 km NE de Saanich, BC"
+  },
+  {
+    "time_utc": "2026-05-11T10:30:23.000Z",
+    "magnitude": "1.68",
     "depth_km": "5.0",
-    "place": "121 km WSW of Haines Junction, YT/121 km OSO de Haines Junction, YT"
+    "place": "137 km W of Haines Junction, YT/137 km O de Haines Junction, YT"
+  },
+  {
+    "time_utc": "2026-05-11T08:56:26.000Z",
+    "magnitude": "1.19",
+    "depth_km": "30.06",
+    "place": "39 km SW of Tahsis, BC/39 km SO de Tahsis, BC"
   }
 ];
