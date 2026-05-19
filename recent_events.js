b36d520b4d690d32c25ba5c19f3559932323c172
Alpeace89 <alpeace89@yahoo.com>
2026-05-19 14:21:05 -0400

    Auto update 2026-05-19 14:21:05

--- recent_events.js
+++ recent_events.js
@@ -1,5 +1,17 @@
 window.recentEvents = [
   {
+    "time_utc": "2026-05-19T14:43:59.000Z",
+    "magnitude": "2.86",
+    "depth_km": "6.27",
+    "place": "127 km SSW of Whitehorse, YT/127 km SSO de Whitehorse, YT"
+  },
+  {
+    "time_utc": "2026-05-19T10:01:11.000Z",
+    "magnitude": "2.53",
+    "depth_km": "10.0",
+    "place": "164 km SW of Port Hardy, BC/164 km SO de Port Hardy, BC"
+  },
+  {
     "time_utc": "2026-05-18T20:39:11.000Z",
     "magnitude": "0.34",
     "depth_km": "5.0",
@@ -10,6 +22,18 @@ window.recentEvents = [
     "magnitude": "2.0",
     "depth_km": "0.0",
     "place": "Blast, 19 km SW of Gogama, ON/Dynamitage, 19 km SO de Gogama, ON"
+  },
+  {
+    "time_utc": "2026-05-16T23:59:39.000Z",
+    "magnitude": "3.06",
+    "depth_km": "10.0",
+    "place": "198 km WSW of Port Hardy, BC/198 km OSO de Port Hardy, BC"
+  },
+  {
+    "time_utc": "2026-05-16T01:18:13.000Z",
+    "magnitude": "2.91",
+    "depth_km": "1.0",
+    "place": "Suspected industry-related event, 30 km S of Woodland Cree 226, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 30 km S de Woodland Cree 226, AB"
   },
   {
     "time_utc": "2026-05-15T23:56:25.000Z",
@@ -34,29 +58,5 @@ window.recentEvents = [
     "magnitude": "2.5",
     "depth_km": "10.0",
     "place": "14 km SSW of Bralorne, BC/14 km SSO de Bralorne, BC"
-  },
-  {
-    "time_utc": "2026-05-15T13:14:41.000Z",
-    "magnitude": "1.33",
-    "depth_km": "32.72",
-    "place": "65 km S of Houpsitas 6, BC/65 km S de Houpsitas 6, BC"
-  },
-  {
-    "time_utc": "2026-05-15T12:20:09.000Z",
-    "magnitude": "2.44",
-    "depth_km": "5.0",
-    "place": "128 km SSW of Whitehorse, YT/128 km SSO de Whitehorse, YT"
-  },
-  {
-    "time_utc": "2026-05-15T05:49:59.000Z",
-    "magnitude": "2.35",
-    "depth_km": "17.96",
-    "place": "70 km S of Daajing Giids, BC/70 km S de Daajing Giids, BC"
-  },
-  {
-    "time_utc": "2026-05-15T02:38:34.000Z",
-    "magnitude": "2.39",
-    "depth_km": "5.0",
-    "place": "125 km WSW of Haines Junction, YT/125 km OSO de Haines Junction, YT"
   }
 ];
