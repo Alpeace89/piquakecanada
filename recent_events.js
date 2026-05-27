a23d8b4908d2b6b95fe24f0aa5f36570ee31e10a
Alpeace89 <alpeace89@yahoo.com>
2026-05-27 16:21:05 -0400

    Auto update 2026-05-27 16:21:05

--- recent_events.js
+++ recent_events.js
@@ -12,6 +12,12 @@ window.recentEvents = [
     "place": "69 km W of Daajing Giids, BC/69 km O de Daajing Giids, BC"
   },
   {
+    "time_utc": "2026-05-27T09:17:13.000Z",
+    "magnitude": "3.31",
+    "depth_km": "18.7",
+    "place": "88 km WSW of Masset, BC/88 km OSO de Masset, BC"
+  },
+  {
     "time_utc": "2026-05-26T08:27:10.000Z",
     "magnitude": "2.79",
     "depth_km": "17.85",
@@ -52,11 +58,5 @@ window.recentEvents = [
     "magnitude": "2.76",
     "depth_km": "1.0",
     "place": "Suspected industry-related event, 69 km S of Grande Prairie, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 69 km S de Grande Prairie, AB"
-  },
-  {
-    "time_utc": "2026-05-25T03:10:14.000Z",
-    "magnitude": "2.26",
-    "depth_km": "5.0",
-    "place": "130 km W of Haines Junction, YT/130 km O de Haines Junction, YT"
   }
 ];
