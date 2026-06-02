fd78e8ec23e7059caccbf6a4f953771884642f6f
Alpeace89 <alpeace89@yahoo.com>
2026-06-02 12:21:07 -0400

    Auto update 2026-06-02 12:21:07

--- recent_events.js
+++ recent_events.js
@@ -6,6 +6,24 @@ window.recentEvents = [
     "place": "19 km SSW of La Malbaie, QC, felt/19 km SSO de La Malbaie, QC, ressenti"
   },
   {
+    "time_utc": "2026-06-01T12:53:40.000Z",
+    "magnitude": "3.57",
+    "depth_km": "5.0",
+    "place": "139 km W of Haines Junction, YT/139 km O de Haines Junction, YT"
+  },
+  {
+    "time_utc": "2026-06-01T11:02:30.000Z",
+    "magnitude": "3.58",
+    "depth_km": "1.0",
+    "place": "Suspected industry-related event, 64 km SSE of Grande Prairie, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 64 km SSE de Grande Prairie, AB"
+  },
+  {
+    "time_utc": "2026-06-01T01:40:40.000Z",
+    "magnitude": "2.5",
+    "depth_km": "5.0",
+    "place": "69 km SSE of Haines Junction, YT/69 km SSE de Haines Junction, YT"
+  },
+  {
     "time_utc": "2026-06-01T01:24:38.000Z",
     "magnitude": "3.02",
     "depth_km": "5.0",
@@ -40,23 +58,5 @@ window.recentEvents = [
     "magnitude": "4.02",
     "depth_km": "25.0",
     "place": "170 km N of Tuktoyaktuk, NT/170 km N de Tuktoyaktuk, NT"
-  },
-  {
-    "time_utc": "2026-05-30T19:36:48.000Z",
-    "magnitude": "3.07",
-    "depth_km": "10.0",
-    "place": "236 km SSW of Port Hardy, BC/236 km SSO de Port Hardy, BC"
-  },
-  {
-    "time_utc": "2026-05-30T16:03:13.000Z",
-    "magnitude": "0.67",
-    "depth_km": "11.65",
-    "place": "14 km ESE of Baie-Saint-Paul, QC/14 km ESE de Baie-Saint-Paul, QC"
-  },
-  {
-    "time_utc": "2026-05-30T15:28:54.000Z",
-    "magnitude": "0.18",
-    "depth_km": "18.0",
-    "place": "21 km N of Matane, QC/21 km N de Matane, QC"
   }
 ];
