6c2dd0cd87ac62e6558329545e02b3320a0f1ffa
Alpeace89 <alpeace89@yahoo.com>
2026-06-02 13:21:03 -0400

    Auto update 2026-06-02 13:21:03

--- recent_events.js
+++ recent_events.js
@@ -18,6 +18,24 @@ window.recentEvents = [
     "place": "Suspected industry-related event, 64 km SSE of Grande Prairie, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 64 km SSE de Grande Prairie, AB"
   },
   {
+    "time_utc": "2026-06-01T08:59:20.000Z",
+    "magnitude": "2.49",
+    "depth_km": "10.0",
+    "place": "72 km SSW of Houpsitas 6, BC/72 km SSO de Houpsitas 6, BC"
+  },
+  {
+    "time_utc": "2026-06-01T06:50:43.000Z",
+    "magnitude": "2.57",
+    "depth_km": "15.4",
+    "place": "61 km W of Daajing Giids, BC/61 km O de Daajing Giids, BC"
+  },
+  {
+    "time_utc": "2026-06-01T03:11:40.000Z",
+    "magnitude": "2.82",
+    "depth_km": "10.0",
+    "place": "74 km SSW of Houpsitas 6, BC/74 km SSO de Houpsitas 6, BC"
+  },
+  {
     "time_utc": "2026-06-01T01:40:40.000Z",
     "magnitude": "2.5",
     "depth_km": "5.0",
@@ -40,23 +58,5 @@ window.recentEvents = [
     "magnitude": "2.71",
     "depth_km": "10.0",
     "place": "195 km W of Port Hardy, BC/195 km O de Port Hardy, BC"
-  },
-  {
-    "time_utc": "2026-05-31T09:46:23.000Z",
-    "magnitude": "2.61",
-    "depth_km": "1.1",
-    "place": "Mining event, 13 km WSW of Kamloops, BC/\u00c9v\u00e9nement minier, 13 km OSO de Kamloops, BC"
-  },
-  {
-    "time_utc": "2026-05-31T00:56:26.000Z",
-    "magnitude": "2.78",
-    "depth_km": "2.19",
-    "place": "Suspected industry-related event, 76 km SSE of Grande Prairie, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 76 km SSE de Grande Prairie, AB"
-  },
-  {
-    "time_utc": "2026-05-30T21:39:20.000Z",
-    "magnitude": "4.02",
-    "depth_km": "25.0",
-    "place": "170 km N of Tuktoyaktuk, NT/170 km N de Tuktoyaktuk, NT"
   }
 ];
