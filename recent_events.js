9abf450c2a023e60c7df67a4c23959efc24495e0
Alpeace89 <alpeace89@yahoo.com>
2026-06-03 19:21:04 -0400

    Auto update 2026-06-03 19:21:04

--- recent_events.js
+++ recent_events.js
@@ -1,5 +1,11 @@
 window.recentEvents = [
   {
+    "time_utc": "2026-06-03T17:40:33.000Z",
+    "magnitude": "2.56",
+    "depth_km": "2.76",
+    "place": "Suspected industry-related event, 41 km SSE of Grande Prairie, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 41 km SSE de Grande Prairie, AB"
+  },
+  {
     "time_utc": "2026-06-03T13:33:03.000Z",
     "magnitude": "3.52",
     "depth_km": "1.0",
@@ -52,11 +58,5 @@ window.recentEvents = [
     "magnitude": "2.66",
     "depth_km": "10.0",
     "place": "238 km SSW of Port Hardy, BC/238 km SSO de Port Hardy, BC"
-  },
-  {
-    "time_utc": "2026-06-02T20:54:58.000Z",
-    "magnitude": "3.59",
-    "depth_km": "3.59",
-    "place": "Suspected industry-related event, 66 km SSE of Grande Prairie, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 66 km SSE de Grande Prairie, AB"
   }
 ];
