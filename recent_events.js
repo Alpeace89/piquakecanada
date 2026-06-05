6e5a1c20068cf335046a5fd493ff7a7acc99c728
Alpeace89 <alpeace89@yahoo.com>
2026-06-05 11:21:03 -0400

    Auto update 2026-06-05 11:21:02

--- recent_events.js
+++ recent_events.js
@@ -18,10 +18,22 @@ window.recentEvents = [
     "place": "Blast, 11 km ESE of Hinton, AB/Dynamitage, 11 km ESE de Hinton, AB"
   },
   {
+    "time_utc": "2026-06-04T07:33:33.000Z",
+    "magnitude": "1.47",
+    "depth_km": "39.45",
+    "place": "6 km E of Tahsis, BC/6 km E de Tahsis, BC"
+  },
+  {
     "time_utc": "2026-06-04T06:48:58.000Z",
     "magnitude": "1.02",
     "depth_km": "11.87",
     "place": "16 km NE of L\u00e9vis, QC/16 km NE de L\u00e9vis, QC"
+  },
+  {
+    "time_utc": "2026-06-04T05:10:31.000Z",
+    "magnitude": "1.84",
+    "depth_km": "67.71",
+    "place": "65 km WNW of Vancouver, BC/65 km ONO de Vancouver, BC"
   },
   {
     "time_utc": "2026-06-04T04:06:06.000Z",
@@ -46,17 +58,5 @@ window.recentEvents = [
     "magnitude": "2.56",
     "depth_km": "2.76",
     "place": "Suspected industry-related event, 41 km SSE of Grande Prairie, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 41 km SSE de Grande Prairie, AB"
-  },
-  {
-    "time_utc": "2026-06-03T13:33:03.000Z",
-    "magnitude": "3.52",
-    "depth_km": "1.0",
-    "place": "104 km SW of Burwash Landing, YT/104 km SO de Burwash Landing, YT"
-  },
-  {
-    "time_utc": "2026-06-03T10:51:42.000Z",
-    "magnitude": "2.58",
-    "depth_km": "10.0",
-    "place": "202 km W of Port Hardy, BC/202 km O de Port Hardy, BC"
   }
 ];
