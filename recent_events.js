89b0d50603c249ee4049d1fa8d1c582067bd2983
Alpeace89 <alpeace89@yahoo.com>
2026-06-09 16:21:03 -0400

    Auto update 2026-06-09 16:21:03

--- recent_events.js
+++ recent_events.js
@@ -6,57 +6,57 @@ window.recentEvents = [
     "place": "Suspected industry-related event, 30 km W of Blueberry River 205, BC/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 30 km O de Blueberry River 205, BC"
   },
   {
+    "time_utc": "2026-06-09T10:18:23.000Z",
+    "magnitude": "2.39",
+    "depth_km": "20.0",
+    "place": "115 km SW of Port Alice, BC/115 km SO de Port Alice, BC"
+  },
+  {
+    "time_utc": "2026-06-09T10:16:57.000Z",
+    "magnitude": "2.92",
+    "depth_km": "20.0",
+    "place": "121 km SSW of Port Alice, BC/121 km SSO de Port Alice, BC"
+  },
+  {
+    "time_utc": "2026-06-09T09:56:11.000Z",
+    "magnitude": "2.2",
+    "depth_km": "20.0",
+    "place": "116 km SW of Port Alice, BC/116 km SO de Port Alice, BC"
+  },
+  {
     "time_utc": "2026-06-09T07:41:11.000Z",
     "magnitude": "2.25",
     "depth_km": "5.0",
     "place": "Suspected industry-related event, 54 km W of Prespatou, BC/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 54 km O de Prespatou, BC"
   },
   {
-    "time_utc": "2026-06-08T07:38:28.000Z",
-    "magnitude": "1.83",
-    "depth_km": "29.15",
-    "place": "58 km ESE of Victoria, BC/58 km ESE de Victoria, BC"
+    "time_utc": "2026-06-09T06:44:17.000Z",
+    "magnitude": "2.63",
+    "depth_km": "20.0",
+    "place": "118 km SW of Port Alice, BC/118 km SO de Port Alice, BC"
   },
   {
-    "time_utc": "2026-06-08T06:46:31.000Z",
-    "magnitude": "2.53",
-    "depth_km": "29.17",
-    "place": "58 km ESE of Victoria, BC/58 km ESE de Victoria, BC"
+    "time_utc": "2026-06-09T05:09:28.000Z",
+    "magnitude": "3.19",
+    "depth_km": "20.0",
+    "place": "118 km SSW of Port Alice, BC/118 km SSO de Port Alice, BC"
   },
   {
-    "time_utc": "2026-06-08T06:43:56.000Z",
-    "magnitude": "3.12",
-    "depth_km": "28.41",
-    "place": "57 km ESE of Victoria, BC, felt/57 km ESE de Victoria, BC, ressenti"
+    "time_utc": "2026-06-08T22:13:09.000Z",
+    "magnitude": "2.74",
+    "depth_km": "5.0",
+    "place": "15 km W of Haines Junction, YT/15 km O de Haines Junction, YT"
   },
   {
-    "time_utc": "2026-06-08T06:19:07.000Z",
-    "magnitude": "2.41",
-    "depth_km": "17.93",
-    "place": "47 km WSW of Daajing Giids, BC/47 km OSO de Daajing Giids, BC"
+    "time_utc": "2026-06-08T15:46:49.000Z",
+    "magnitude": "3.23",
+    "depth_km": "16.39",
+    "place": "50 km WSW of Daajing Giids, BC/50 km OSO de Daajing Giids, BC"
   },
   {
-    "time_utc": "2026-06-08T06:05:08.000Z",
-    "magnitude": "2.15",
-    "depth_km": "29.01",
-    "place": "37 km ESE of Masset, BC/37 km ESE de Masset, BC"
-  },
-  {
-    "time_utc": "2026-06-07T22:07:52.000Z",
-    "magnitude": "2.39",
-    "depth_km": "0.0",
-    "place": "Blast, 15 km SW of Trend Mine, BC/Dynamitage, 15 km SO de la mine Trend, BC"
-  },
-  {
-    "time_utc": "2026-06-07T21:48:45.000Z",
-    "magnitude": "2.07",
-    "depth_km": "0.0",
-    "place": "Blast, 14 km SE of Mount Milligan Mine, BC/Dynamitage, 14 km SE de la mine Mount Milligan, BC"
-  },
-  {
-    "time_utc": "2026-06-07T19:18:38.000Z",
-    "magnitude": "0.52",
-    "depth_km": "3.36",
-    "place": "10 km E of Baie-Saint-Paul, QC/10 km E de Baie-Saint-Paul, QC"
+    "time_utc": "2026-06-08T12:04:50.000Z",
+    "magnitude": "2.5",
+    "depth_km": "5.0",
+    "place": "120 km WSW of Haines Junction, YT/120 km OSO de Haines Junction, YT"
   }
 ];
