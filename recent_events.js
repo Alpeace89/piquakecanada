0d120a93ce3f001491dfc10587d984a22a8d917d
Alpeace89 <alpeace89@yahoo.com>
2026-06-10 17:21:06 -0400

    Auto update 2026-06-10 17:21:06

--- recent_events.js
+++ recent_events.js
@@ -1,5 +1,11 @@
 window.recentEvents = [
   {
+    "time_utc": "2026-06-10T18:32:52.000Z",
+    "magnitude": "2.67",
+    "depth_km": "1.15",
+    "place": "Suspected industry-related event, 74 km SSE of Grande Prairie, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 74 km SSE de Grande Prairie, AB"
+  },
+  {
     "time_utc": "2026-06-10T13:17:14.000Z",
     "magnitude": "3.52",
     "depth_km": "1.0",
@@ -16,6 +22,18 @@ window.recentEvents = [
     "magnitude": "2.92",
     "depth_km": "10.0",
     "place": "119 km SSW of Port Alice, BC/119 km SSO de Port Alice, BC"
+  },
+  {
+    "time_utc": "2026-06-10T07:12:23.000Z",
+    "magnitude": "2.4",
+    "depth_km": "1.0",
+    "place": "Suspected industry-related event, 75 km SSE of Grande Prairie, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 75 km SSE de Grande Prairie, AB"
+  },
+  {
+    "time_utc": "2026-06-10T01:25:10.000Z",
+    "magnitude": "2.42",
+    "depth_km": "15.81",
+    "place": "119 km SSE of Daajing Giids, BC/119 km SSE de Daajing Giids, BC"
   },
   {
     "time_utc": "2026-06-09T20:24:26.000Z",
@@ -40,23 +58,5 @@ window.recentEvents = [
     "magnitude": "2.39",
     "depth_km": "20.0",
     "place": "115 km SW of Port Alice, BC/115 km SO de Port Alice, BC"
-  },
-  {
-    "time_utc": "2026-06-09T10:16:57.000Z",
-    "magnitude": "2.92",
-    "depth_km": "20.0",
-    "place": "121 km SSW of Port Alice, BC/121 km SSO de Port Alice, BC"
-  },
-  {
-    "time_utc": "2026-06-09T09:56:11.000Z",
-    "magnitude": "2.2",
-    "depth_km": "20.0",
-    "place": "116 km SW of Port Alice, BC/116 km SO de Port Alice, BC"
-  },
-  {
-    "time_utc": "2026-06-09T07:41:11.000Z",
-    "magnitude": "2.25",
-    "depth_km": "5.0",
-    "place": "Suspected industry-related event, 54 km W of Prespatou, BC/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 54 km O de Prespatou, BC"
   }
 ];
