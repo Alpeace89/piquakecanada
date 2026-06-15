c41334044eda8253b7aadce072bef8fcc20b9097
Alpeace89 <alpeace89@yahoo.com>
2026-06-15 16:21:05 -0400

    Auto update 2026-06-15 16:21:05

--- recent_events.js
+++ recent_events.js
@@ -1,15 +1,33 @@
 window.recentEvents = [
   {
+    "time_utc": "2026-06-15T12:32:52.000Z",
+    "magnitude": "1.36",
+    "depth_km": "0.5",
+    "place": "Mining event, Niobec Mine, QC/\u00c9v\u00e9nement minier, Mine Niobec, QC"
+  },
+  {
     "time_utc": "2026-06-15T12:23:10.000Z",
-    "magnitude": "2.82",
-    "depth_km": "1.0",
-    "place": "Suspected mining event, Niobec Mine, QC, felt/\u00c9v\u00e9nement minier soupconn\u00e9, Mine Niobec, QC, ressenti"
+    "magnitude": "2.8",
+    "depth_km": "0.5",
+    "place": "Mining event, Niobec Mine, QC, felt/\u00c9v\u00e9nement minier, Mine Niobec, QC, ressenti"
   },
   {
     "time_utc": "2026-06-14T22:40:43.000Z",
     "magnitude": "1.48",
     "depth_km": "14.68",
     "place": "12 km NNW of Saint-Pascal, QC/12 km NNO de Saint-Pascal, QC"
+  },
+  {
+    "time_utc": "2026-06-13T13:20:28.000Z",
+    "magnitude": "3.08",
+    "depth_km": "10.0",
+    "place": "156 km W of Tofino, BC/156 km O de Tofino, BC"
+  },
+  {
+    "time_utc": "2026-06-13T12:42:52.000Z",
+    "magnitude": "3.53",
+    "depth_km": "10.0",
+    "place": "249 km SSW of Port Hardy, BC/249 km SSO de Port Hardy, BC"
   },
   {
     "time_utc": "2026-06-13T12:37:34.000Z",
@@ -40,23 +58,5 @@ window.recentEvents = [
     "magnitude": "2.67",
     "depth_km": "1.0",
     "place": "Suspected industry-related event, 64 km SSE of Grande Prairie, AB/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 64 km SSE de Grande Prairie, AB"
-  },
-  {
-    "time_utc": "2026-06-13T08:24:24.000Z",
-    "magnitude": "2.07",
-    "depth_km": "29.09",
-    "place": "57 km ESE of Victoria, BC/57 km ESE de Victoria, BC"
-  },
-  {
-    "time_utc": "2026-06-13T07:11:01.000Z",
-    "magnitude": "0.51",
-    "depth_km": "0.5",
-    "place": "Mining event, Niobec Mine, QC/\u00c9v\u00e9nement minier, Mine Niobec, QC"
-  },
-  {
-    "time_utc": "2026-06-13T06:52:56.000Z",
-    "magnitude": "0.7",
-    "depth_km": "0.5",
-    "place": "Mining event, Niobec Mine, QC/\u00c9v\u00e9nement minier, Mine Niobec, QC"
   }
 ];
