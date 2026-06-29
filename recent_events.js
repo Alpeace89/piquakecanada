8a19e0025bc801a942418e3e9ef00506f6899dce
Alpeace89 <alpeace89@yahoo.com>
2026-06-29 09:21:05 -0400

    Auto update 2026-06-29 09:21:05

--- recent_events.js
+++ recent_events.js
@@ -1,5 +1,17 @@
 window.recentEvents = [
   {
+    "time_utc": "2026-06-28T21:46:34.000Z",
+    "magnitude": "1.87",
+    "depth_km": "15.47",
+    "place": "15 km E of La Malbaie, QC/15 km E de La Malbaie, QC"
+  },
+  {
+    "time_utc": "2026-06-26T06:35:21.000Z",
+    "magnitude": "0.6",
+    "depth_km": "35.29",
+    "place": "13 km WSW of Houpsitas 6, BC/13 km OSO de Houpsitas 6, BC"
+  },
+  {
     "time_utc": "2026-06-25T19:53:03.000Z",
     "magnitude": "1.32",
     "depth_km": "0.0",
@@ -46,17 +58,5 @@ window.recentEvents = [
     "magnitude": "2.56",
     "depth_km": "1.0",
     "place": "Suspected industry-related event, 25 km S of Halfway River 168, BC/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 25 km S de Halfway River 168, BC"
-  },
-  {
-    "time_utc": "2026-06-24T17:42:35.000Z",
-    "magnitude": "3.2",
-    "depth_km": "5.0",
-    "place": "107 km SSW of Old Crow, YT/107 km SSO de Old Crow, YT"
-  },
-  {
-    "time_utc": "2026-06-24T13:16:25.000Z",
-    "magnitude": "2.52",
-    "depth_km": "1.0",
-    "place": "167 km SSW of Whitehorse, YT/167 km SSO de Whitehorse, YT"
   }
 ];
