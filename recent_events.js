8cf6a3c9c0698a546c5f1f0dba2b44c252caba21
Alpeace89 <alpeace89@yahoo.com>
2026-06-30 15:21:03 -0400

    Auto update 2026-06-30 15:21:03

--- recent_events.js
+++ recent_events.js
@@ -12,10 +12,34 @@ window.recentEvents = [
     "place": "349 km ESE of Cape Breton, NS/349 km ESE de Cape Breton, NS"
   },
   {
+    "time_utc": "2026-06-29T21:51:59.000Z",
+    "magnitude": "2.26",
+    "depth_km": "5.0",
+    "place": "8 km SSE of Doaktown, NB/8 km SSE de Doaktown, NB"
+  },
+  {
+    "time_utc": "2026-06-29T05:28:07.000Z",
+    "magnitude": "1.76",
+    "depth_km": "21.97",
+    "place": "18 km ESE of North Bay, ON/18 km ESE de North Bay, ON"
+  },
+  {
     "time_utc": "2026-06-28T21:46:34.000Z",
     "magnitude": "1.87",
     "depth_km": "15.47",
     "place": "15 km E of La Malbaie, QC/15 km E de La Malbaie, QC"
+  },
+  {
+    "time_utc": "2026-06-28T17:22:18.000Z",
+    "magnitude": "2.88",
+    "depth_km": "5.0",
+    "place": "100 km SSW of Old Crow, YT/100 km SSO de Old Crow, YT"
+  },
+  {
+    "time_utc": "2026-06-28T15:41:33.000Z",
+    "magnitude": "2.63",
+    "depth_km": "4.3",
+    "place": "Suspected industry-related event, 34 km NNW of Halfway River 168, BC/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 34 km NNO de Halfway River 168, BC"
   },
   {
     "time_utc": "2026-06-28T06:52:34.000Z",
@@ -34,29 +58,5 @@ window.recentEvents = [
     "magnitude": "2.15",
     "depth_km": "1.0",
     "place": "Suspected industry-related event, 18 km S of Halfway River 168, BC/\u00c9v\u00e9nement li\u00e9 \u00e0 l'industrie soupconn\u00e9, 18 km S de Halfway River 168, BC"
-  },
-  {
-    "time_utc": "2026-06-26T10:36:10.000Z",
-    "magnitude": "3.32",
-    "depth_km": "10.0",
-    "place": "257 km WNW of Port Hardy, BC/257 km ONO de Port Hardy, BC"
-  },
-  {
-    "time_utc": "2026-06-26T08:06:55.000Z",
-    "magnitude": "2.4",
-    "depth_km": "13.24",
-    "place": "44 km E of Seattle, WA/44 km E de Seattle, WA"
-  },
-  {
-    "time_utc": "2026-06-26T06:35:21.000Z",
-    "magnitude": "0.6",
-    "depth_km": "35.29",
-    "place": "13 km WSW of Houpsitas 6, BC/13 km OSO de Houpsitas 6, BC"
-  },
-  {
-    "time_utc": "2026-06-25T21:42:24.000Z",
-    "magnitude": "2.0",
-    "depth_km": "1.3",
-    "place": "134 km W of Haines Junction, YT/134 km O de Haines Junction, YT"
   }
 ];
