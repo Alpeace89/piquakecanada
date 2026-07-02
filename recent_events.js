7ece0b8aa450a991008c84f34f1717d47d5b0f9b
Alpeace89 <alpeace89@yahoo.com>
2026-07-02 10:21:06 -0400

    Auto update 2026-07-02 10:21:06

--- recent_events.js
+++ recent_events.js
@@ -36,6 +36,18 @@ window.recentEvents = [
     "place": "15 km SW of Gibsons, BC/15 km SO de Gibsons, BC"
   },
   {
+    "time_utc": "2026-06-29T09:25:28.000Z",
+    "magnitude": "2.3",
+    "depth_km": "9.88",
+    "place": "19 km SW of Masset, BC/19 km SO de Masset, BC"
+  },
+  {
+    "time_utc": "2026-06-29T08:01:04.000Z",
+    "magnitude": "1.68",
+    "depth_km": "31.93",
+    "place": "78 km SW of Tahsis, BC/78 km SO de Tahsis, BC"
+  },
+  {
     "time_utc": "2026-06-29T05:28:07.000Z",
     "magnitude": "1.76",
     "depth_km": "21.97",
@@ -46,17 +58,5 @@ window.recentEvents = [
     "magnitude": "1.87",
     "depth_km": "15.47",
     "place": "15 km E of La Malbaie, QC/15 km E de La Malbaie, QC"
-  },
-  {
-    "time_utc": "2026-06-28T17:22:18.000Z",
-    "magnitude": "2.88",
-    "depth_km": "5.0",
-    "place": "100 km SSW of Old Crow, YT/100 km SSO de Old Crow, YT"
-  },
-  {
-    "time_utc": "2026-06-28T16:29:06.000Z",
-    "magnitude": "2.68",
-    "depth_km": "10.0",
-    "place": "230 km SSW of Port Hardy, BC/230 km SSO de Port Hardy, BC"
   }
 ];
