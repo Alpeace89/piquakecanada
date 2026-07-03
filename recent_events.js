f1aa3c5b9687c8986fcfc80651ff43e38eb8c0f5
Alpeace89 <alpeace89@yahoo.com>
2026-07-03 10:21:05 -0400

    Auto update 2026-07-03 10:21:05

--- recent_events.js
+++ recent_events.js
@@ -12,6 +12,24 @@ window.recentEvents = [
     "place": "Blast, 5 km E of Saint-Jovite, QC/Dynamitage, 5 km E de Saint-Jovite, QC"
   },
   {
+    "time_utc": "2026-07-02T11:22:12.000Z",
+    "magnitude": "1.31",
+    "depth_km": "16.41",
+    "place": "36 km ESE of Victoria, BC/36 km ESE de Victoria, BC"
+  },
+  {
+    "time_utc": "2026-07-02T10:50:54.000Z",
+    "magnitude": "1.96",
+    "depth_km": "27.96",
+    "place": "57 km ESE of Victoria, BC/57 km ESE de Victoria, BC"
+  },
+  {
+    "time_utc": "2026-07-02T10:03:39.000Z",
+    "magnitude": "1.79",
+    "depth_km": "1.0",
+    "place": "81 km SW of Lohbiee 3, BC/81 km SO de Lohbiee 3, BC"
+  },
+  {
     "time_utc": "2026-07-02T09:49:48.000Z",
     "magnitude": "1.01",
     "depth_km": "22.22",
@@ -40,23 +58,5 @@ window.recentEvents = [
     "magnitude": "2.19",
     "depth_km": "29.55",
     "place": "58 km ESE of Victoria, BC/58 km ESE de Victoria, BC"
-  },
-  {
-    "time_utc": "2026-07-02T06:24:52.000Z",
-    "magnitude": "1.78",
-    "depth_km": "3.52",
-    "place": "48 km SW of Lohbiee 3, BC/48 km SO de Lohbiee 3, BC"
-  },
-  {
-    "time_utc": "2026-07-02T05:58:50.000Z",
-    "magnitude": "2.41",
-    "depth_km": "2.18",
-    "place": "32 km W of Squamish, BC/32 km O de Squamish, BC"
-  },
-  {
-    "time_utc": "2026-07-02T03:50:37.000Z",
-    "magnitude": "2.2",
-    "depth_km": "10.0",
-    "place": "241 km SSW of Port Hardy, BC/241 km SSO de Port Hardy, BC"
   }
 ];
